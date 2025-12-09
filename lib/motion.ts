db6f9eddfe387447cc7a0516e323c34414f8e1e7
Nico Haubold <n.haubold29@gmail.com>
2025-12-09 21:50:51 +0200

    feat: animation timing tokens setup

---
+++ lib/motion.ts
@@ -0,0 +1,12 @@
+export const motion = {
+  fast: 0.4,
+  medium: 0.8,
+  slow: 1.2,
+
+  easeOut: "power3.out",
+  easeInOut: "power2.inOut",
+
+  staggerSm: 0.05,
+  staggerMd: 0.12,
+  staggerLg: 0.2,
+};
