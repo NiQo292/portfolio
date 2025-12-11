54129d0a4bfe0bffb6b8759ec65255925d64b51f
Nico Haubold <n.haubold29@gmail.com>
2025-12-11 17:25:59 +0200

    style: reorder class names for consistency in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,17 +17,17 @@ const navLinks = {
 
 const Footer = () => {
   return (
-    <footer className="bg-black z-100">
+    <footer className="z-100 bg-black">
       <div
         className="relative h-[600px]"
         style={{ clipPath: "polygon(0% 0, 100% 0, 100% 100%, 0 100%)" }}
       >
         <div className="fixed bottom-0 h-[600px] w-full">
-          <div className="py-8 px-12 h-full w-full flex flex-col justify-between text-white">
+          <div className="flex h-full w-full flex-col justify-between px-12 py-8 text-white">
             <div>
               <div className="flex shrink-0 gap-20">
                 <div className="flex flex-col gap-2">
-                  <h3 className="mb-2 uppercase text-[#ffffff80]">About</h3>
+                  <h3 className="mb-2 text-[#ffffff80] uppercase">About</h3>
                   {navLinks.about.map((link) => (
                     <Link key={link.name} href={link.href}>
                       <TextUpDown>{link.name}</TextUpDown>
@@ -35,7 +35,7 @@ const Footer = () => {
                   ))}
                 </div>
                 <div className="flex flex-col gap-2">
-                  <h3 className="mb-2 uppercase text-[#ffffff80]">Socials</h3>
+                  <h3 className="mb-2 text-[#ffffff80] uppercase">Socials</h3>
                   {navLinks.socials.map((link) => (
                     <Link key={link.name} href={link.href}>
                       <TextUpDown>{link.name}</TextUpDown>
@@ -44,8 +44,8 @@ const Footer = () => {
                 </div>
               </div>
             </div>
-            <div className="flex justify-between items-end">
-              <h1 className="text-[12vw] leading-[0.8] mt-10">Nico Haubold</h1>
+            <div className="flex items-end justify-between">
+              <h1 className="mt-10 text-[12vw] leading-[0.8]">Nico Haubold</h1>
               <p className="">&copy; Copyright</p>
             </div>
           </div>
