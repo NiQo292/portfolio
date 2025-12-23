b9da68cda454554bd10256e26f655f1e4b4e7154
Nico Haubold <n.haubold29@gmail.com>
2025-12-24 00:44:51 +0100

    feat: add SpeedInsights component and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { clashDisplay, satoshi } from "./fonts";
 import SmoothScrollProvider from "@/components/SmoothScrollProvider";
@@ -34,6 +35,7 @@ export default function RootLayout({
             <Footer />
           </div>
         </SmoothScrollProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
